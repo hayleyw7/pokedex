08d2792519f3e6aa5e281d08bb0ce4219d14ad7d
Matt Roden <mattroden92@gmail.com>
2021-09-10 17:38:00 -0600

    Make apiCalls file - get and set data to state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { getPokedexData } from './apiCalls'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokeDex: []
+    }
+  }
+
+  componentDidMount() {
+    getPokedexData()
+    .then(data => {
+      this.setState({
+        pokeDex: data.results
+      })
+    })
+  }
+
+  render() {
+    return(
+      <div>Hello World</div>
+    )
+  }
 }
 
-export default App;
+export default App
