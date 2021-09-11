b26b252ad1043e9fe419c26a33a25d28f65eb082
Matt Roden <mattroden92@gmail.com>
2021-09-11 17:06:48 -0600

    Successfully validate queried input data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,23 +23,30 @@ class App extends Component {
     })
   }
 
+  validatePokemonData = (queriedPokemon) => {
+    const lowerCaseInput = queriedPokemon.queriedPokemon.toLowerCase()
+
+    let nameCheck = this.state.pokeDex.find(pokemon => {
+      let lowerCaseName = pokemon.name.toLowerCase()
+      if (lowerCaseName.includes(lowerCaseInput) && lowerCaseInput !== '')  {
+        return pokemon
+      }
+    })
+
+    if (nameCheck === undefined) {
+      console.log('No Good!')
+    } else {
+      console.log(nameCheck) 
+    }
+  }
+
   addPokemon = (queriedPokemon) => {
-    console.log(queriedPokemon)
-    return this.validatePokemonData(queriedPokemon)
+    this.validatePokemonData(queriedPokemon)
     //set state with the validated pokemon
   }
 
 
-  validatePokemonData = (queriedPokemon) => {
-    const lowerCaseInput = queriedPokemon.queriedPokemon.toLowerCase()
-    return console.log(lowerCaseInput, '<><>')
-    // make all data.names in pokedex to lower case
-    // const validatedPokemon =
-    // make queiredPokemon lower case before validating
-    // validating our data to make sure the queriedSearch is infact in our getPokedexData
-    // compares name or id (arr.length)
-    // if validated, fires addPokemon with the validatedPokemon as the argument
-  }
+
 
   render() {
     return(
