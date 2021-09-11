0edfb2042bd968f52568d9a754a30ac29cfabecb
Matt Roden <mattroden92@gmail.com>
2021-09-11 14:58:24 -0600

    Change foundPokemon to queriedPokemon in Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import './Search.css'
 
 
 class Search  extends Component  {
     constructor() {
         super();
         this.state = {
-            foundPokemon:''
+            queriedPokemon:''
         }
     }
 
@@ -16,25 +16,25 @@ class Search  extends Component  {
 
     handleClick = (e) => {
         e.preventDefault();
-        const foundPokemon = {
+        const queriedPokemon = {
             id:Date.now(),
             ...this.state
         }
-        console.log(foundPokemon)
-        // foundPokemon will be = data.name 
+        console.log(queriedPokemon)
+        // foundPokemon will be = data.name
     }
 
     render() {
         return (
             <form>
-                <input 
-                    type="search" 
-                    name="foundPokemon" 
-                    className="search-bar" 
-                    placeholder="Choose your Pokemon" 
+                <input
+                    type="search"
+                    name="foundPokemon"
+                    className="search-bar"
+                    placeholder="Choose your Pokemon"
                     onChange={(e) => this.handleChange(e)}/>
-                <button 
-                    type="submit" 
+                <button
+                    type="submit"
                     onClick={(e) => this.handleClick(e)}>
                 Catch
                 </button>
