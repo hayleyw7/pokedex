e0e3be19a9838cdf7bbb5b653f7f6c448a7ec3d9
Joana Brito <joanafiorentini@hotmail.com>
2021-09-12 16:49:55 -0700

    Add getPokemonImage method and imported PokedexGrid

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Search from '../Search/Search';
-import { getPokedexData } from '../../apiCalls'
-
+import { getPokedexData } from '../../apiCalls';
+import PokedexGrid from '../PokedexGrid/PokedexGrid';
 import './App.css';
 
 class App extends Component {
@@ -41,10 +41,10 @@ class App extends Component {
     if (verifiedName === undefined) {
       console.log('No Good NAMe!')
     } else {
-      let pokemon_id = verifiedName.url.replace(/\D/g, "").slice(1);
-      let pokemon_image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon_id}.png`
-      console.log(verifiedName.url)
-      console.log(pokemon_image)
+      // let pokemon_id = verifiedName.url.replace(/\D/g, "").slice(1);
+      // let pokemon_image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon_id}.png`
+      // console.log(verifiedName.url)
+      // console.log(pokemon_image)
       console.log(verifiedName, 'IT WORKSSSSSSS!!!!')
       return verifiedName
     }
@@ -52,10 +52,21 @@ class App extends Component {
 
   // pass the validated query through addPokemon
 
+
+  getPokemonImage = (id) => {
+    
+    let pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+   
+    return pokemonImage
+
+  }
+
+
   render() {
     return(
-      <div>Hello World
+      <div> Hello World
         <Search addPokemon={this.addPokemon}/>
+        <PokedexGrid pokedexData={this.state.pokeDex} getPokemonImage={this.getPokemonImage}/>
       </div>
     )
   }
