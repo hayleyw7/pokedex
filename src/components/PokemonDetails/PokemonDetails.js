6a303797d99d6e1b8d4a449911f2e0a42b1a04d6
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-13 16:22:16 -0600

    Add abilities

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -9,39 +9,29 @@ const PokemonDetails = ({ foundPokemonH }) => {
       <h1>{foundPokemonH.id}: {foundPokemonH.name} </h1>
 
       <h2>Moves:</h2>
-      <article className='moves-container'>
+      <article className='features-container'>
         {foundPokemonH.moves.map(move =>
-          <p className='moves' key={move.move.name}>{move.move.name}</p>
+          <p className='features' key={move.move.name}>{move.move.name}</p>
         )}
       </article>
 
       <h2>Types:</h2>
-      <article className='types-container'>
+      <article className='features-container'>
         {foundPokemonH.types.map(type =>
-          <p className='types' key={type.type.name}>{type.type.name}</p>
+          <p className='features' key={type.type.name}>{type.type.name}</p>
         )}
       </article>
+
+      <h2>Abilities:</h2>
+
+      <article className='features-container'>
+        {foundPokemonH.abilities.map(ability =>
+          <p className='features' key={ability.ability.name}>{ability.ability.name}</p>
+        )}
+      </article>      
 
     </div>
   )
 }
 
 export default PokemonDetails;
-
-      // <article className='typesList'>
-      //   {foundPokemonH.types.map(type =>
-      //     <p className='types' key={type.type.name}>{type.type.name}</p>
-      //   )}
-      // </article>
-
-      // <article className='abilitiesList'>
-      //   {foundPokemonH.abilities.map(ability =>
-      //     <p className='abilities' key={ability.ability.name}>{ability.ability.name}</p>
-      //   )}
-      // </article>
-
-      // <article className='movesList'>
-      //   {foundPokemonH.moves.map(move =>
-      //     <p className='moves' key={move.move.name}>{move.move.name}</p>
-      //   )}
-      // </article>
