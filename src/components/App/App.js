becd220b730b2b18f273f8d7cee9e9306d9ddc44
Matt Roden <mattroden92@gmail.com>
2021-09-13 16:28:39 -0600

    Add Header component files and render jsx to screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Search from '../Search/Search';
 import { getPokedexData } from '../../apiCalls';
 import PokedexGrid from '../PokedexGrid/PokedexGrid';
+import Header from '../Header/Header'
 import './App.css';
 
 class App extends Component {
@@ -34,7 +35,7 @@ class App extends Component {
 
   validatePokemonData = (queriedPokemon) => {
     const lowerCaseInput = queriedPokemon.queriedPokemon.toLowerCase()
-    
+
     const verifiedName = this.state.pokeDex.find((pokemon, index) => {
       let lowerCaseName = pokemon.name.toLowerCase()
 
@@ -52,19 +53,19 @@ class App extends Component {
     if (verifiedName === undefined) {
       return this.setState({ error: "Not  a valid name, try again" })
       console.log('No Good Name!')
-    } else { 
+    } else {
       console.log(verifiedName, 'IT WORKSSSSSSS!!!!')
       return verifiedName
     }
-   
+
   }
 
   // pass the validated query through addPokemon
 
   getPokemonImage = (id) => {
-    
+
     let pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
-   
+
     return pokemonImage
 
   }
@@ -74,13 +75,16 @@ class App extends Component {
     const text = 'Loading...';
 
     return(
-      <div> Welcome to PoKeDeX, are you ready to catch your Pokemon?
+      <div className='App'>
+
+        <Header />
+
         <Search addPokemon={this.addPokemon}/>
         {(this.state.foundPokemon.length === 0 && !this.state.error) && <h2>{ text }</h2>}
         {(this.state.error && <h2> { this.state.error }</h2>)}
-        {(this.state.foundPokemon.length !== 0 && !this.state.error)&& 
+        {(this.state.foundPokemon.length !== 0 && !this.state.error)&&
         <PokedexGrid pokedexData={this.state.foundPokemon} getPokemonImage={this.getPokemonImage}/>}
-        {(this.state.foundPokemon.length === 0) && 
+        {(this.state.foundPokemon.length === 0) &&
         <PokedexGrid pokedexData={this.state.pokeDex} getPokemonImage={this.getPokemonImage}/>
         }
       </div>
@@ -90,8 +94,8 @@ class App extends Component {
 
 export default App;
 
-// pseudocde about how to add the images to the main page 
-// let pokemon_id = verifiedName.url.replace(/\D/g, "").slice(1);- it was showing as 225 because it has 
+// pseudocde about how to add the images to the main page
+// let pokemon_id = verifiedName.url.replace(/\D/g, "").slice(1);- it was showing as 225 because it has
       //  the V2 as a number as well in the url , to fix that i've used slice(1), to take away the 1 number
       // let pokemon_image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon_id}.png`
       // console.log(verifiedName.url)
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>Header Goes Here!</div>
+  )
+}
+
+export default Header
