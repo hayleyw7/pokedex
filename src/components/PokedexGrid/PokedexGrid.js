a48efe50309fc17559da2b547505db9ca1d6e041
Joana Brito <joanafiorentini@hotmail.com>
2021-09-12 17:02:10 -0700

    Import PokemonCard to the grid

--- src/components/PokedexGrid/PokedexGrid.js
+++ src/components/PokedexGrid/PokedexGrid.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PokemonCard from '../PokemonCard/PokemonCard';
 // import '/PokedexGrid.css';
 
 
 const PokedexGrid = (props) => {
 
     const myPokemon = props.pokedexData.map(pokemon => {
+        
         let pokemonId = pokemon.url.replace(/\D/g, "").slice(1)
         let pokemonImage = props.getPokemonImage(pokemonId);
 
