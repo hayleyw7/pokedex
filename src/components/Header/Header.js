db651ac7f3fc09a54453a426d14de8236d5e4c3a
Matt Roden <mattroden92@gmail.com>
2021-09-13 17:23:56 -0600

    Remove assets folder - pull image from public - add ash to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
+// import ash from '/Images/ash1.png
 import './Header.css'
-
+//Assets/Images/ash1.png
 const Header = () => {
   return (
     <header className='header'>
-    <div class="custom-shape-divider-bottom-1631572219">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
-      </svg>
-    </div>
+
+      <img className='header-image' src='/Images/ash1.png' alt='ash-ketchum'/>
+      <div class="custom-shape-divider-bottom-1631572219">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+        </svg>
+      </div>
     </header>
   )
 }
