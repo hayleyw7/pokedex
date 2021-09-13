9cbc523eadb3738813bb2e943b858c6c17dcb55f
Matt Roden <mattroden92@gmail.com>
2021-09-13 16:54:19 -0600

    Add saped header and color gradient to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,13 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header Goes Here!</div>
+    <header className='header'>
+    <div class="custom-shape-divider-bottom-1631572219">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+      </svg>
+    </div>
+    </header>
   )
 }
 
