33e9ab3a07290ff8ad0969d2f8ee87f58cafa926
Matt Roden <mattroden92@gmail.com>
2021-09-13 22:06:34 -0600

    Successfully add image of Ash back into header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
         </ul>
       </nav>
 
-      <img src='Images/ash1.png' alt='ash-ketchum'/>
+      <img className='header-image' src='Images/ash1.png' alt='ash-ketchum'/>
 
       <div class="custom-shape-divider-bottom-1631589819">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
