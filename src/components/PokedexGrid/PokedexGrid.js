188181726f4e032a9837d17c3db0e24325af3e43
Joana Brito <joanafiorentini@hotmail.com>
2021-09-14 22:52:22 -0700

    Add pseudocode

--- src/components/PokedexGrid/PokedexGrid.js
+++ src/components/PokedexGrid/PokedexGrid.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import PokemonCard from '../PokemonCard/PokemonCard';
-import './PokedexGrid.css';
-
+import React from "react";
+import PokemonCard from "../PokemonCard/PokemonCard";
+import "./PokedexGrid.css";
 
 const PokedexGrid = (props) => {
-    const myPokemon = props.pokedexData.map(pokemon => {
-
-        let pokemonId = pokemon.url.replace(/\D/g, "").slice(1)
-        let pokemonImage = props.getPokemonImage(pokemonId);
-
-        return (
-            <PokemonCard 
-                key={pokemonId}
-                id={pokemonId}
-                image={pokemonImage}
-                name={pokemon.name}
-            />           
-        )
-    });
+  const myPokemon = props.pokedexData.map((pokemon) => {
+    let pokemonId = pokemon.url.replace(/\D/g, "").slice(1);
+    let pokemonImage = props.getPokemonImage(pokemonId);
 
     return (
-        <div className="all-pokemon">
-            { myPokemon }
-        </div>
+      <PokemonCard
+        key={pokemonId}
+        id={pokemonId}
+        image={pokemonImage}
+        name={pokemon.name}
+        // onClick={()=> getPokemonDetaisl({pokemonId})}
+      />
     );
+  });
+
+  return <div className="all-pokemon">{myPokemon}</div>;
 };
-    
-    
+
 export default PokedexGrid;
