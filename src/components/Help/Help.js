dde41100f55f5b97786a99cefe64c1371e4b956e
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-15 14:34:33 -0600

    Get router working for how-to

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Help.css';
 
+
 const Help = () => {
   return (
     <div className='help-page'>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Search.css'
 
+
 class Search  extends Component  {
     constructor(props) {
         super(props);
