d1993d6174980b63d03f03015410652033774837
Joana Brito <joanafiorentini@hotmail.com>
2021-09-14 22:28:12 -0700

    Add hooks and useEffect to be able to access the data

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -1,51 +1,83 @@
+import React, { useState, useEffect } from "react";
+import "./PokemonDetails.css";
 
-import React from 'react';
-import './PokemonDetails.css';
+const PokemonDetails = ({ foundPokemon, getPokemonImage }) => {
+  const [pokemonDetails, setPokemonDetails] = useState([]);
+  const [error, setError] = useState("");
 
-const PokemonDetails = ({ foundPokemonH, getPokemonImage }) => {
+  const pokemonImage = getPokemonImage(
+    foundPokemon[0].url.replace(/\D/g, "").slice(1)
+  );
 
-  let pokemonImage = getPokemonImage(foundPokemonH.id);
+  const getPokemonDetails = async () => {
+    let pokemonId = foundPokemon[0].url.replace(/\D/g, "").slice(1);
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
 
-  return (
-    <div className='pokemon-details-page'>
-      <div className='pokemon-details-container'>  
+    try {
+      const res = await fetch(url);
+      const pokemonDetails = await res.json();
+      setPokemonDetails(pokemonDetails);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
-        <h1 className='pokemon-details-id'>{foundPokemonH.id}</h1>
+  useEffect(() => {
+    getPokemonDetails();
+  }, []);
 
-        <h1 className='pokemon-details-header capitalize'>{foundPokemonH.name} </h1>
+  if (pokemonDetails.types) {
+    return (
+      <div className="pokemon-details-page">
+        <div className="pokemon-details-container">
+          <h1 className="pokemon-details-id">{pokemonDetails.id}</h1>
 
-        <img
-          src={pokemonImage}      className='single-pokemon-pic'
-          alt={`${foundPokemonH.name} image`}
-          id={foundPokemonH.id}
-        />        
+          <h1 className="pokemon-details-header capitalize">
+            {pokemonDetails.name}{" "}
+          </h1>
 
-        <h2 className='types-header'>Type</h2>
-        <article className='features-container'>
-          {foundPokemonH.types.map(type =>
-            <p className='features' key={type.type.name}>{type.type.name.split('-').join(' ')}</p>
-          ).sort((elementA, elementB) => elementA - elementB)}
-        </article>
+          <img
+            src={pokemonImage}
+            className="single-pokemon-pic"
+            alt={`${pokemonDetails.name} image`}
+            id={pokemonDetails.id}
+          />
 
-        <h2 className='abilities-header'>Abilities</h2>
+          <h2 className="types-header">Type</h2>
+          <article className="features-container">
+            {pokemonDetails.types
+              .map((type) => (
+                <p className="features" key={type.type.name}>
+                  {type.type.name.split("-").join(" ")}
+                </p>
+              ))
+              .sort((elementA, elementB) => elementA - elementB)}
+          </article>
 
-        <article className='features-container'>
-          {foundPokemonH.abilities.map(ability =>
-            <p className='features' key={ability.ability.name}>{ability.ability.name.split('-').join(' ')}</p>
-          )}
-        </article>   
+          <h2 className="abilities-header">Abilities</h2>
 
-        <h2 className='moves-header'>Moves</h2>
-        <article className='features-container'>
-          {foundPokemonH.moves.map(move =>
-            <p className='features' key={move.move.name}>{move.move.name.split('-').join(' ')}</p>
-          )}
-        </article>        
-   
+          <article className="features-container">
+            {pokemonDetails.abilities.map((ability) => (
+              <p className="features" key={ability.ability.name}>
+                {ability.ability.name.split("-").join(" ")}
+              </p>
+            ))}
+          </article>
 
+          <h2 className="moves-header">Moves</h2>
+          <article className="features-container">
+            {pokemonDetails.moves.map((move) => (
+              <p className="features" key={move.move.name}>
+                {move.move.name.split("-").join(" ")}
+              </p>
+            ))}
+          </article>
+        </div>
       </div>
-    </div>  
-  )
-}
+    );
+  } else {
+    return <h2 className="moves-header">Loading...</h2>;
+  }
+};
 
 export default PokemonDetails;
