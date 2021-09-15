21ff3edc8f9b52ef8fd2164055bf52f5dd22c527
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-15 08:27:14 -0600

    Start Help component

---
+++ src/components/Help/Help.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Help.css';
+
+const Help = () => {
+  return (
+    <div className='help-page'>
+      <div className='help-container'>  
+        <h1 className='help-header'>How To Use Your PoKedeX</h1>   
+
+        <h2 className='help-search-header'>Search Pokemon by ID or Name</h2>
+        <article className='help-search-container'>
+
+        
+        
+          <p>help</p>
+        </article>
+
+        <h2 className='help-pokemon-header'>Learn about a Pokemon</h2>
+        <article className='help-pokemon-container'>
+          <p>help</p>
+        </article>
+
+        <h2 className='help-3-header'>what should we put here?</h2>
+        <article className='help-3-container'>
+          <p>help</p>
+        </article>      
+      </div>
+    </div>  
+  )
+}
+
+export default Help;
