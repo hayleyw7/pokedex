beeba4040c9a763ab51196fa76dd17088f39792f
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-15 08:49:33 -0600

    Add help info

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -10,22 +10,25 @@ const Help = () => {
         <h2 className='help-search-header'>Search Pokemon by ID or Name</h2>
         <article className='help-search-container'>
 
-        
-        
-          <p>help</p>
-        </article>
+          <p>
+            The PoKedeX is a Progressive Web Application that gives a user the ability to search through Generation 1 Pokemon and quickly find information on them. If you encounter a Pokemon in the wild, and you need to find quick information about it, even without internet service, this is the app for you!
+          </p>
+          <p>
+            When you visit the app on a computer or tablet, you should be able to see a seach box and a display of all Pokemon by ID number. On a mobile device, you should just see the search box.
+            
+          <p>
+          </p>
+            From there the home page, you can the search box to access details about individual Generation 1 Pokemon by name or ID, including image, type, moves, and abilities. And if you're on desktop or a tablet, in additition, you also an image of a Pokemon to access the same details.
+          </p>
+          <p>
+            From either this how-to page or the Pokemon details page, you can return to the home page by clicking the "Back" button.
+          </p>
 
-        <h2 className='help-pokemon-header'>Learn about a Pokemon</h2>
-        <article className='help-pokemon-container'>
-          <p>help</p>
-        </article>
+          <button type="button">Back</button>
 
-        <h2 className='help-3-header'>what should we put here?</h2>
-        <article className='help-3-container'>
-          <p>help</p>
-        </article>      
+        </article>
       </div>
-    </div>  
+    </div>
   )
 }
 
