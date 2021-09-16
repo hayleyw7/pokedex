e3ac0f7a9379a70f95f629a54691237df3549f22
Matt Roden <mattroden92@gmail.com>
2021-09-16 17:37:53 -0600

    Comment out template service worker code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const router =
   <BrowserRouter>
@@ -16,4 +16,4 @@ ReactDOM.render(router, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 // serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
