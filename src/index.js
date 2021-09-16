07377528bf68fd2169f6e4a597eebafa8cb6e1cd
Matt Roden <mattroden92@gmail.com>
2021-09-16 10:15:19 -0600

    Add register/unregister functions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 
-const router = 
+const router =
   <BrowserRouter>
     <App />
   </BrowserRouter>
 
 ReactDOM.render(router, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
