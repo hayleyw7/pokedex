f592eb628eea23b7815e31e1340a86a3522449c0
Matt Roden <mattroden92@gmail.com>
2021-09-17 08:42:01 -0600

    Begin service worker file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,1 +1,12 @@
-console.log('hello from serviceworker.js')
+const cache_name = 'version-1'
+const urlsToCache = [ 'index.html']
+const self = this
+
+// Install switch
+self.addEventListener('install', () =>? {
+  
+})
+
+// listen for requests
+
+// activate SW
