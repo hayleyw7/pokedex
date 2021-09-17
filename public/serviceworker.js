033f4d0bd2b40549606a44980f27155ad217861d
Matt Roden <mattroden92@gmail.com>
2021-09-17 09:26:49 -0600

    Successfully open Cache

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -3,15 +3,22 @@ const urlsToCache = [ 'index.html']
 const self = this
 
 // Install switch
-self.addEventListener('install', (event) =>? {
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cache_name)
+      .then((cache) => {
+        console.log('Opened Cache')
 
+        return cache.addAll(urlsToCache)
+      })
+  )
 })
 
 // listen for requests
-self.addEventListener('fetch', (event) =>? {
+self.addEventListener('fetch', (event) => {
 
 })
 // activate SW
-self.addEventListener('activate', (event) =>? {
+self.addEventListener('activate', (event) => {
 
 })
