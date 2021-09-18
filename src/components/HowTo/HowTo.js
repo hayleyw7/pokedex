594dcf96ae5cdd3da00c3af44977258a20230f71
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-18 15:31:37 -0600

    Change mew to meowth

--- src/components/HowTo/HowTo.js
+++ src/components/HowTo/HowTo.js
@@ -59,8 +59,8 @@ const HowTo = (props) => {
           </p>
 
         </article>
-        <section className='mew-quote'>
-           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/151.svg" className="mew-pic" alt="mew"/>
+        <section className='meowth-quote'>
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/52.svg" className="mew-pic" alt="meowth"/>
            
            <p>"We do have a lot in common. The same earth, the same air, the same sky. Maybe if we started looking at what is the same, instead of looking at what is different, well, who knows?"</p>
         </section>
