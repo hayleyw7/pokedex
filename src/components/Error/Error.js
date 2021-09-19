481a7d545768e4d77076116d2626653c257456bd
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-19 17:37:26 -0600

    Start Error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Error.css';
+
+const Error = () => {
+  return (
+    <div className='error-container'>
+      <img
+        alt='Jigglypuff Error'
+        className='jigglypuff-error'
+        src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/39.svg'
+      ></img>
+      <div className='error-message'>
+        <h2 className='error-quote'>"Everybody makes a wrong turn once in a while!"</h2>
+        <p className='error-note'>Please enter a valid Generation 1 Pokemon name or ID.</p>
+      </div>
+    </div> 
+  )
+}
+
+export default Error;
