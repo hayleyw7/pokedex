83e3ea3e66dfacdb4316cd301f9073d721933b5e
Matt Roden <mattroden92@gmail.com>
2021-09-19 12:16:04 -0600

    Comment out sw registration import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const router =
   <BrowserRouter>
