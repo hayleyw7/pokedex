34736dd12ccd24938b33740b120cb97a53d3d137
Matt Roden <mattroden92@gmail.com>
2021-09-19 20:29:10 -0600

    Animate elements to fly on screen from offscreen left

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Loader.css'
+import { gsap } from 'gsap'
 
 const Loader = () => {
+  let loadingMessage = useRef(null)
+  let pokeball = useRef(null)
+
+  useEffect(() => {
+    gsap.from([loadingMessage, pokeball], 0.25, {
+      delay: 0.15,
+      ease: "power3.out",
+      x: -1164,
+      stagger: {
+        amount: 0.15
+      }
+    })
+
+  }, [loadingMessage, pokeball])
 
   return (
     <section className='loader-container'>
-      <h1 className='loader-text'>Just a moment please!</h1>
-      <img src='Images/pokeball.png' className='loader-image' alt='pokeball loading icon'/>
+      <h1 ref={el => loadingMessage = el} className='loader-text'>Just a moment please!</h1>
+      <img src='Images/pokeball.png' ref={el => pokeball = el} className='loader-image' alt='pokeball loading icon'/>
     </section>
   )
 };
