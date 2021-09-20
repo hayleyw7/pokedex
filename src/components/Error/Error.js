3539565946cea30e39923b73c38b0c565ac22525
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-19 19:11:21 -0600

    Rm unused import and change ID

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Error.css';
-import { gsap, Power3 } from 'gsap';
 
 const Error = () => {
 
@@ -13,7 +12,7 @@ const Error = () => {
       ></img>
       <div className='error-message'>
         <h2 className='error-quote'>"Everybody makes a wrong turn once in a while!"</h2>
-        <p className='error-note'>Try again. Enter a real Generation 1 Pokemon name or ID (1 to 152).</p>
+        <p className='error-note'>Try again. Enter a real Generation 1 Pokemon name or ID (1 to 151).</p>
       </div>
     </div>     
   )
