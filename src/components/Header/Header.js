43955c8bf45837f80ca40577822cdc2f38418092
Joana Brito <joanafiorentini@hotmail.com>
2021-09-19 22:45:35 -0700

    Start animation with pokeball

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Header.css'
 import { Route, Link } from 'react-router-dom';
+import { gsap } from 'gsap';
 
-const Header = ( clearPokemon ) => {
+const Header = ({ foundPokemon }) => {
+
+  useEffect(()=> {
+    gsap.fromTo('.pokeball', 
+    { 
+      opacity: 1,
+      // scale: -3,
+      duration: 2, 
+      ease: "rough({ template: none.out, strength: 1, points: 20, taper: 'none', randomize: true, clamp: false})", 
+      y: 0, 
+      scale: 2.5,
+      rotation: 720
+    },
+    {   
+        scale: 1,
+        opacity: 1,
+        duration: 2.5, 
+        ease: "rough({ template: none.out, strength: 1, points: 20, taper: 'none', randomize: true, clamp: false})", 
+        y: 0,
+       
+        rotation: 0
+    })
+  }, [ foundPokemon]);
 
   return (
     <header className='header'>
