e4753f34a21f7b00aea0d4f5d2d242ed672282fb
Matt Roden <mattroden92@gmail.com>
2021-09-19 21:09:14 -0600

    Rename cache

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,6 @@
 // the cache version gets updated every time there is a new deployment
-const CACHE_VERSION = 10;
-const CURRENT_CACHE = `main-${CACHE_VERSION}`;
+const CACHE_VERSION = 1;
+const CURRENT_CACHE = `main-Cache-${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
 const cacheFiles = [
