9064f2502a83779acd17418e0d150fd972b77bc9
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-29 09:31:20 -0600

    Simplify how to

--- src/components/HowTo/HowTo.js
+++ src/components/HowTo/HowTo.js
@@ -68,11 +68,6 @@ const HowTo = (props) => {
           </p>
 
         </article>
-        <section className='meowth-quote'>
-           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/52.svg" className="mew-pic" alt="meowth"/>
-           
-           <p>"We do have a lot in common: the same earth, the same air, the same sky. Maybe if we started looking at what is the same, instead of looking at what is different, well, who knows?"</p>
-        </section>
       </div>
     </div>
   )
